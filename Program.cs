9cdb0e6c3ec597f001ced464952744f2a114af09
Sebastian-Cristian Ciuta <sebastian-cristian.ciuta@etud.u-picardie.fr>
2024-03-18 14:12:37 +0100

    Ajout saisie clavier

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿int carre(int i){
     return i * i;
 }
-
-Console.WriteLine(carre(4));
+Console.WriteLine("Saisir un entier");
+int val = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(carre(val));
